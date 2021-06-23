e6f5675b3ca787f6636db3771097569163eef5d8
Tristan Stenner <ttstenner@gmail.com>
2021-06-23 17:04:16 +0200

    Write build info to configuration header file to avoid unescaped quotes on the command line

--- src/buildinfo.cpp
+++ src/buildinfo.cpp
@@ -1,11 +1,15 @@
 extern "C" {
 #include "../include/lsl/common.h"
 
-LIBLSL_C_API const char *lsl_library_info(void) {
-#ifdef LSL_LIBRARY_INFO_STR
-	return LSL_LIBRARY_INFO_STR;
+#ifdef LSL_CMAKE_INFO
+#include "buildinfo.h"
 #else
-	return "Unknown (not set by build system)";
+#ifndef LSL_LIBRARY_INFO_STR
+#define LSL_LIBRARY_INFO_STR "Unknown (not set by build system)"
 #endif
+#endif
+
+LIBLSL_C_API const char *lsl_library_info(void) {
+	return LSL_LIBRARY_INFO_STR;
 }
 }

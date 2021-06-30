157da54a14a897b9d2cb2ae3bbc9eb00fae79b69
Tristan Stenner <ttstenner@gmail.com>
2021-06-30 16:17:08 +0200

    SendStringMarkers: send markers with offsets to check if lsl time is used properly

--- examples/SendStringMarkers.cpp
+++ examples/SendStringMarkers.cpp
@@ -8,6 +8,11 @@
  * This example program offers a 1-channel stream which contains strings.
  * The stream has the "Marker" content type and irregular rate.
  * The name of the stream can be chosen as a startup parameter.
+ *
+ * Each marker is sent three times: with a negative time offset, "immediate"
+ * and with a positive time offset, i.e. in the future.
+ * Compliant stream visualizers / recording software should display those
+ * markers exactly 150ms apart
  */
 
 int main(int argc, char* argv[]) {
@@ -25,16 +30,22 @@ int main(int argc, char* argv[]) {
 		std::random_device rd;
 		std::mt19937 gen(rd());
 		std::uniform_int_distribution<std::size_t> rnd(0, markertypes.size() - 1);
-		std::uniform_int_distribution<int> delayrnd(0, 1000);
+		std::uniform_int_distribution<int> delayrnd(400, 1000);
 		while(true) {
-			// wait for a 20ms
-			std::this_thread::sleep_for(std::chrono::milliseconds(delayrnd(gen)));
+			// wait for a variable delay
+			auto delay = std::chrono::milliseconds(delayrnd(gen));
+			std::this_thread::sleep_for(delay);
 			// and choose the marker to send
 			std::string mrk = markertypes[rnd(gen)];
-			std::cout << "now sending: " << mrk << std::endl;
-
-			// now send it (note the &)
-			outlet.push_sample(&mrk);
+			std::cout << "now sending: 3x " << mrk << std::endl;
+			double t = lsl::local_clock();
+			for (double offset : {-.2, 0., .2}) {
+				std::string sendmarker = mrk + ' ' + std::to_string(offset);
+				// now send it (note the &)
+				outlet.push_sample(&mrk, t + offset);
+				std::this_thread::sleep_for(std::chrono::milliseconds(5));
+				std::cout << "now sending: " << sendmarker << std::endl;
+			}
 		}
 	} catch (std::exception& e) { std::cerr << "Got an exception: " << e.what() << std::endl; }
 	std::cout << "Press any key to exit. " << std::endl;
